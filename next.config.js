b25dc41bbc9006e70417f2c4288b016a9d298caf
jovaniq1 <jovaniq1@gmail.com>
2022-08-11 15:46:00 -0500

    varcel

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: false,
   env: {
     customKey: 'my-value',
   },
-  assetPrefix: isProd
-    ? 'https://cdn.statically.io/gh/jovaniq1/jovaniq1.github.io/gh-pages/'
-    : '',
 };
 
 module.exports = nextConfig;
